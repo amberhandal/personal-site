0c77869ff7dbe5940997ad91c24295902575be9d
Amber Handal <amberhandal24@gmail.com>
2024-01-24 17:24:11 -0500

    link

--- components/intro.tsx
+++ components/intro.tsx
@@ -93,7 +93,7 @@ export default function Intro() {
 
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          ref="@/public/Amber-Handal-Resume.pdf"
+          href="@/public/Amber-Handal-Resume.pdf"
           download
         >
           Download resume{" "}
@@ -102,7 +102,7 @@ export default function Intro() {
 
         <a
           className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://linkedin.com"
+          href="https://www.linkedin.com/in/amberhandal/"
           target="_blank"
         >
           <BsLinkedin />
@@ -110,7 +110,7 @@ export default function Intro() {
 
         <a
           className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://github.com"
+          href="https://github.com/amberhandal"
           target="_blank"
         >
           <FaGithubSquare />
