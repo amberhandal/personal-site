4f51ff6aeece8759d70d39343c53d0310f8e1533
Amber Handal <amberhandal24@gmail.com>
2024-02-08 20:04:01 -0500

    update pdf

--- next.config.js
+++ next.config.js
@@ -1,13 +1,47 @@
-/** @type {import('next').NextConfig} */
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     images: {
+//         remotePatterns: [
+//             {
+//                 protocol: "https",
+//                 hostname: 'images.prismic.io',
+//             }
+//         ]
+//     }
+// }
+
+// module.exports = nextConfig
+// next.config.js
 const nextConfig = {
     images: {
-        remotePatterns: [
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: 'images.prismic.io',
+        },
+      ],
+    },
+    webpack: (config, { isServer }) => {
+      if (!isServer) {
+        // Add PDF handling
+        config.module.rules.push({
+          test: /\.pdf$/,
+          use: [
             {
-                protocol: "https",
-                hostname: 'images.prismic.io',
-            }
-        ]
-    }
-}
-
-module.exports = nextConfig
+              loader: 'file-loader',
+              options: {
+                name: '[path][name].[ext]',
+                publicPath: '/_next/static/files',
+                outputPath: 'static/files',
+              },
+            },
+          ],
+        });
+      }
+  
+      return config;
+    },
+  };
+  
+  module.exports = nextConfig;
+  
