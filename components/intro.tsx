3aa6d0078d80a9b14ae7028afe1179f0c73e72eb
Amber Handal <amberhandal24@gmail.com>
2024-02-08 20:04:01 -0500

    update pdf

--- components/intro.tsx
+++ components/intro.tsx
@@ -93,8 +93,8 @@ export default function Intro() {
 
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-            href={require("@/public/Amber-Handal-Resume.pdf")}
-            download="Amber-Handal-Resume.pdf"
+          href="/Amber-Handal-Resume.pdf" // Direct link to the PDF in the public directory
+          download="Amber-Handal-Resume.pdf"
         >
           Download resume{" "}
           <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
