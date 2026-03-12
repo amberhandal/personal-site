7071e7976ff797748d7e25ac4ae598562abbe5a2
amberhandal <ambahjh@gmail.com>
2026-03-12 00:51:23 -0500

    fix project cards

--- components/project.tsx
+++ components/project.tsx
@@ -39,7 +39,7 @@ export default function Project({
         <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
           <h3 className="text-2xl font-semibold">{title}</h3>
 
-          <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70">
+          <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70 line-clamp-3">
             {description}
           </p>
 
